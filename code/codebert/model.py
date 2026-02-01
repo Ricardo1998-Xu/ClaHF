f9fdddf3c62c48ae8d3c23c7ba17206e69b9bcc5
Ricardo <161216581+Ricardo1998-Xu@users.noreply.github.com>
2026-02-01 16:37:06 +0800

    Add files via upload

---
+++ code/codebert/model.py
@@ -0,0 +1,96 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+import torch
+import torch.nn as nn
+import torch
+from torch.autograd import Variable
+import copy
+from torch.nn import CrossEntropyLoss, MSELoss
+class FocalLoss(nn.Module):
+    def __init__(self, alpha=1, gamma=2, reduction='mean'):
+        super(FocalLoss, self).__init__()
+        self.alpha = alpha
+        self.gamma = gamma
+        self.reduction = reduction
+
+    def forward(self, input, target):
+        ce_loss = nn.CrossEntropyLoss(reduction='none')(input, target)
+        pt = torch.exp(-ce_loss)
+        focal_loss = self.alpha * (1 - pt) ** self.gamma * ce_loss
+
+        if self.reduction == 'mean':
+            return focal_loss.mean()
+        elif self.reduction == 'sum':
+            return focal_loss.sum()
+        else:
+            return focal_loss
+
+
+class Model(nn.Module):
+    def __init__(self, encoder, config, tokenizer, args):
+        super(Model, self).__init__()
+        self.encoder = encoder
+        self.config = config
+        self.tokenizer = tokenizer
+        self.args = args
+        self.classifier = nn.Linear(self.encoder.config.hidden_size, args.num_labels)
+        self.value_head = nn.Linear(self.encoder.config.hidden_size, 1)
+        # Define dropout layer, dropout_probability is taken from args.
+        self.dropout = nn.Dropout(args.dropout_probability)
+
+    def forward(self, input_ids=None, labels=None):
+        attention_mask = input_ids.ne(self.tokenizer.pad_token_id)
+        outputs = self.encoder(input_ids, attention_mask=attention_mask, output_hidden_states=True)
+        hidden_states = outputs.hidden_states[-1]
+        hidden_states = self.dropout(hidden_states)
+
+
+        last_token_logits = hidden_states[:, -1, :]  # (batch_size, hidden_size)
+
+
+        logits = self.classifier(last_token_logits)  # (batch_size, 2)
+        values = self.value_head(last_token_logits).squeeze(-1)
+
+        prob = nn.functional.softmax(logits, dim=-1)
+        if labels is not None:
+            loss_fct = nn.CrossEntropyLoss()
+            # loss_fct = FocalLoss()
+            # loss_fct = FocalLoss_Weight(alpha=class_weights)
+            loss = loss_fct(logits, labels)
+            return loss, prob, values
+        else:
+            return prob, values
+
+
+class CloneModel(nn.Module):
+    def __init__(self, encoder, config, tokenizer, args):
+        super(CloneModel, self).__init__()
+        self.encoder = encoder
+        self.config = config
+        self.tokenizer = tokenizer
+        self.args = args
+
+        self.classifier = nn.Linear(self.encoder.config.hidden_size, args.num_labels)  # 输出二分类
+        self.value_head = nn.Linear(self.encoder.config.hidden_size, 1)
+        # Define dropout layer, dropout_probability is taken from args.
+        self.dropout = nn.Dropout(args.dropout_probability)
+
+    def forward(self, input_ids=None, labels=None):
+        attention_mask = input_ids.ne(self.tokenizer.pad_token_id)
+        outputs = self.encoder(input_ids, attention_mask=attention_mask, output_hidden_states=True)
+        hidden_states = outputs.hidden_states[-1]
+        hidden_states = self.dropout(hidden_states)
+
+        cls_logits = hidden_states[:, 0, :]  # (B, H)
+
+        logits = self.classifier(cls_logits)  # (batch_size, 2)
+        values = self.value_head(cls_logits).squeeze(-1)
+
+        prob = nn.functional.softmax(logits, dim=-1)
+        if labels is not None:
+            loss_fct = nn.CrossEntropyLoss()
+            loss = loss_fct(logits, labels)
+            return loss, prob, values
+        else:
+            return prob, values
+
